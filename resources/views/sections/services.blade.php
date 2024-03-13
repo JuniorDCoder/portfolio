ee4021c3179a34a9499bd732a2fbabda8317b373
JuniorDCoder <foryoungjuniorngu@gmail.com>
2024-03-13 17:17:02 +0100

    feat: Aninmate Portfolio Section

--- resources/views/sections/services.blade.php
+++ resources/views/sections/services.blade.php
@@ -7,55 +7,77 @@
       </div>
 
       <div class="row">
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-          <div class="icon-box">
-            <div class="icon"><i class="bx bx-code"></i></div>
-            <h4><a href="">Full Stack Development</a></h4>
-            <p>Developing end-to-end web applications using the latest technologies and frameworks.</p>
-          </div>
-        </div>
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="icon-box">
+                <div class="icon"><i class="bx bx-code"></i></div>
+                <h4><a href="">Full Stack Development</a></h4>
+                <p>Developing end-to-end web applications using the latest technologies and frameworks.</p>
+            </div>
+            </div>
 
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-          <div class="icon-box">
-            <div class="icon"><i class="bx bx-cog"></i></div>
-            <h4><a href="">Technical Leadership</a></h4>
-            <p>Leading tech teams and overseeing all technical aspects of software development projects.</p>
-          </div>
-        </div>
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="icon-box">
+                <div class="icon"><i class="bx bx-cog"></i></div>
+                <h4><a href="">Technical Leadership</a></h4>
+                <p>Leading tech teams and overseeing all technical aspects of software development projects.</p>
+            </div>
+            </div>
 
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
-          <div class="icon-box">
-            <div class="icon"><i class="bx bx-server"></i></div>
-            <h4><a href="">Backend Development</a></h4>
-            <p>Building and maintaining the server side of web applications, ensuring high performance and responsiveness.</p>
-          </div>
-        </div>
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+                <div class="icon"><i class="bx bx-server"></i></div>
+                <h4><a href="">Backend Development</a></h4>
+                <p>Building and maintaining the server side of web applications, ensuring high performance and responsiveness.</p>
+            </div>
+            </div>
 
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-          <div class="icon-box">
-            <div class="icon"><i class="bx bx-laptop"></i></div>
-            <h4><a href="">Frontend Development</a></h4>
-            <p>Creating interactive and user-friendly web interfaces using modern web technologies.</p>
-          </div>
-        </div>
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+                <div class="icon"><i class="bx bx-laptop"></i></div>
+                <h4><a href="">Frontend Development</a></h4>
+                <p>Creating interactive and user-friendly web interfaces using modern web technologies.</p>
+            </div>
+            </div>
 
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-          <div class="icon-box">
-            <div class="icon"><i class="bx bx-database"></i></div>
-            <h4><a href="">Database Management</a></h4>
-            <p>Designing, implementing, and managing databases to store, organize, and access data efficiently.</p>
-          </div>
-        </div>
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+                <div class="icon"><i class="bx bx-database"></i></div>
+                <h4><a href="">Database Management</a></h4>
+                <p>Designing, implementing, and managing databases to store, organize, and access data efficiently.</p>
+            </div>
+            </div>
 
-        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
-          <div class="icon-box">
-            <div class="icon"><i class="bx bx-network-chart"></i></div>
-            <h4><a href="">IT Support</a></h4>
-            <p>Providing IT solutions and support, maintaining and improving IT infrastructure.</p>
-          </div>
-        </div>
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+                <div class="icon"><i class="bx bx-network-chart"></i></div>
+                <h4><a href="">IT Support</a></h4>
+                <p>Providing IT solutions and support, maintaining and improving IT infrastructure.</p>
+            </div>
+            </div>
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+                <div class="icon-box" data-aos="fade-up">
+                    <div class="icon"><i class="bx bx-code"></i></div>
+                    <h4><a href="">Frontend Development</a></h4>
+                    <p>Creating interactive and user-friendly web interfaces using modern web technologies.</p>
+                </div>
+            </div>
+            <!-- Add more services here -->
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+                <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+                    <div class="icon"><i class="bx bx-database"></i></div>
+                    <h4><a href="">Database Management</a></h4>
+                    <p>Designing, implementing, and managing databases to store, organize, and access data efficiently.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+                <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
+                    <div class="icon"><i class="bx bx-mobile"></i></div>
+                    <h4><a href="">Mobile App Development</a></h4>
+                    <p>Building cross-platform and native mobile applications for iOS and Android platforms.</p>
+                </div>
+            </div>
 
-      </div>
+        </div>
 
     </div>
   </section>
